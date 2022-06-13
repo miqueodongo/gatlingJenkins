ba6cf838252b1b34a8dbfce47e728b69a4c6f5e8
miqueodongo <miqueodongo@gmail.com>
2022-06-13 16:37:41 +0300

    Modified global assertions

--- src/test/scala/finalSimulation/VideoGameFullTest.scala
+++ src/test/scala/finalSimulation/VideoGameFullTest.scala
@@ -108,8 +108,8 @@ class VideoGameFullTest extends Simulation {
     .protocols(httpConf)
     .maxDuration(testDuration.seconds)
     .assertions(
-      global.responseTime.max.lt(2),
-      global.successfulRequests.percent.gt(99)
+      global.responseTime.max.lt(200),
+      global.successfulRequests.percent.gt(95)
     )
 
   /*** After ***/
